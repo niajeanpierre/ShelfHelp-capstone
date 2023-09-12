99479c92b81d781d3effbdd1c01ee2a8c3f2e294
Mackenzie Jessup <Mackenzie.jessup@mykenzie.snhu.edu>
2023-09-12 10:26:36 -0400

    added update and delete endpoints

--- server/src/routes/books.js
+++ server/src/routes/books.js
@@ -36,4 +36,36 @@ router.post('/:bookid', async (req, res, next) => {
         }
     })
 
+    router.put("/update/:bookid", async (req, res, next) => {
+        const { bookid } = req.params
+        let {category, update } = req.body
+
+        try {
+            if (category === "category"){
+                await Book.updateOne({bookid: bookid}, {$set: {category: update}})
+            }else if (category === "notes"){
+                await Book.updateOne({bookid: bookid}, {$set: {notes: update}})
+            }else if (category === "quotes"){
+                await Book.updateOne({bookid: bookid}, {$set: {quotes: update}})
+            }else if (category === "review"){
+                await Book.updateOne({bookid: bookid}, {$set: {review: update}})
+            } else {
+                res.send("can not update category").end()
+            }
+        } catch (error) {
+            res.json({error: error}).end()
+        }
+        let book = await Book.findOne({bookid: bookid})
+        res.json(book)
+    })
+
+    router.delete("/delete/:bookid", async (req,res,next) => {
+        const { bookid } = req.params
+        try {
+            await Book.findOneAndDelete({bookid: bookid})
+        } catch (error) {
+            res.json({error: error}).end()
+        }
+        res.send(`deleted book ${bookid}`)
+    })
 module.exports = router
