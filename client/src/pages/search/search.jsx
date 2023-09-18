be8cb8413c1de97bfbfc19b63316763625371229
kendalljk <120734396+kendalljk@users.noreply.github.com>
2023-09-18 07:24:47 -0700

    fix background, search on submit

--- client/src/pages/search/search.jsx
+++ client/src/pages/search/search.jsx
@@ -5,84 +5,84 @@ import "./search.css";
 import { Form, FormControl, Button } from "react-bootstrap";
 import axios from "axios";
 
-
 const Search = () => {
-  const navigate = useNavigate();
-  const { query } = useParams();
-  const [radioOption, setRadioOption] = useState("title");
-  const [search, setSearch] = useState("");
-  const [filteredBooks, setFilteredBooks] = useState();
+    const navigate = useNavigate();
+    const { query } = useParams();
+    const [radioOption, setRadioOption] = useState("title");
+    const [search, setSearch] = useState("");
+    const [filteredBooks, setFilteredBooks] = useState();
 
-  console.log(query);
+    console.log(query);
 
-  const handleSearch = (e) => {
-    e.preventDefault();
-    axios
-      .get(`https://openlibrary.org/search.json?${radioOption}=${search}`)
-      .then((response) => {
-        // Handle the API response here
-        console.log("API Response:", response.data);
-        setFilteredBooks(response.data.docs);
-      })
-      .catch((error) => {
-        // Handle errors here
-        console.error("API Error:", error);
-      });
-  };
-  useEffect(() => {
-    console.log(radioOption);
-    console.log(search);
-  }, [radioOption, search, filteredBooks]);
+    const handleSearch = (e) => {
+        e.preventDefault();
+        axios
+            .get(`https://openlibrary.org/search.json?${radioOption}=${search}`)
+            .then((response) => {
+                // Handle the API response here
+                console.log("API Response:", response.data);
+                setFilteredBooks(response.data.docs);
+            })
+            .catch((error) => {
+                // Handle errors here
+                console.error("API Error:", error);
+            });
+    };
+    useEffect(() => {
+        console.log(radioOption);
+        console.log(search);
+    }, [radioOption, search, filteredBooks]);
 
-  useEffect(() => {
-    if (query !== undefined) {
-      setSearch(query);
-    }
-  }, [query]);
-  return (
-    <section className="display">
-      <h1>Search</h1>
-      <Form className="d-flex align-items-center">
-        <FormControl
-          type="search"
-          className="searchParams1 nav-search w-100"
-          placeholder="search books..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <Button onClick={handleSearch}>Search</Button>
+    useEffect(() => {
+        if (query !== undefined) {
+            setSearch(query);
+        }
+    }, [query]);
+    return (
+      <section className="display landing d-flex flex-column align-items-center w-100">
 
-        {["title", "author"].map((type) => (
-          <div key={`inline-${type}`} className="mb-3">
-            <Form.Check
-              inline
-              label={type === "title" ? "Title" : "Author"}
-              name="group1"
-              type="radio"
-              id={`inline-${type}-1`}
-              checked={radioOption === type}
-              onChange={() => setRadioOption(type)}
-            />
-          </div>
-        ))}
-      </Form>
-
-      <div>
-        {filteredBooks
-          && filteredBooks.map((book, index) => (
-              <div key={index}>
-                <h4>
-                  {book.title} || Author:{book.author_name}
-                </h4>
-                <img
-                  src={`https://covers.openlibrary.org/b/id/${book.cover_i}-S.jpg`}
-                  alt={`Cover for ${book.title}`}
+            <h1>Search</h1>
+            <Form className="d-flex align-items-center" onSubmit={handleSearch}>
+                <FormControl
+                    type="search"
+                    className="searchParams1 nav-search w-100"
+                    placeholder="search books..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
                 />
-              </div>
-            ))}
-      </div>
-    </section>
-  );
+                <Button type="submit">Search</Button>
+
+                {["title", "author"].map((type) => (
+                    <div key={`inline-${type}`} className="mb-3">
+                        <Form.Check
+                            inline
+                            label={type === "title" ? "Title" : "Author"}
+                            name="group1"
+                            type="radio"
+                            id={`inline-${type}-1`}
+                            checked={radioOption === type}
+                            onChange={() => setRadioOption(type)}
+                        />
+                    </div>
+                ))}
+            </Form>
+
+            <div>
+                {filteredBooks &&
+                    filteredBooks.map((book, index) => (
+                        <div key={index}>
+                            <h4>
+                                {book.title} || Author:{book.author_name}
+                            </h4>
+                            <img
+                                src={`https://covers.openlibrary.org/b/id/${book.cover_i}-S.jpg`}
+                                alt={`Cover for ${book.title}`}
+                            />
+                        </div>
+                    ))}
+            </div>
+        </section>
+    );
 };
 
 export default Search;
--- client/src/pages/tbr/tbr.jsx
+++ client/src/pages/tbr/tbr.jsx
@@ -1,75 +1,40 @@
-import React from "react";
-import cover from "../../images/cover.png";
-import BookDisplay from "../../components/ShelfDisplay";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ShelfDisplay from "../../components/ShelfDisplay";
 
 const TBR = () => {
+    const [books, setBooks] = useState([]);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:3001/api/books"
+                );
+                setBooks(response.data);
+            } catch (error) {
+                console.error("An error occurred while fetching data: ", error);
+            }
+        };
 
-  const books = [
-    {
-      title: "Harry Potter and the Philosopher's stone",
-      author: "JK Rowling",
-      cover: cover,
-      category: "read",
-    },
-    {
-      title: "Harry Potter and the Chamber of Secrets",
-      author: "JK Rowling",
-      cover: cover,
-      category: "read",
-    },
-    {
-      title: "Harry Potter and the Prisoner of Azkaban",
-      author: "JK Rowling",
-      cover: cover,
-      category: "read",
-    },
-    {
-      title: "Harry Potter and the Goblet of Fire",
-      author: "JK Rowling",
-      cover: cover,
-      category: "read",
-    },
-    {
-      title: "Harry Potter and the Order of the Phoenix",
-      author: "JK Rowling",
-      cover: cover,
-      category: "read",
-    },
-    {
-      title: "Harry Potter and the Half-Blood Prince",
-      author: "JK Rowling",
-      cover: cover,
-      category: "reading",
-    },
-    {
-      title: "Harry Potter and the Deathly Hallows",
-      author: "JK Rowling",
-      cover: cover,
-      category: "tbr",
-    },
-  ];
+        fetchData();
+    }, []);
 
-  const tbrBooks = books.filter((book) => book.category === "tbr");
+    const tbrBooks = books.filter((book) => book.category === "tbr");
 
-  return (
-    <section className="display tbr">
+    return (
+        <section className="display tbr">
             <div className="d-flex flex-column">
-                <h2 className="shelf-heading mt-1 mx-2 fst-italic fw-normal">
+                <h2 className="shelf-heading mt-5 mx-5 fst-italic fw-normal">
                     To be read...
                 </h2>
                 <div className="reading mt-2 d-flex flex-wrap">
                     {tbrBooks.map((book, i) => (
                         <div
                             key={i}
-                            className="text-center col-sm-2 col-lg-1 mx-4"
+                            className="text-center d-flex  flex-column align-items-center col-sm-2 col-lg-1 mx-5 mt-3"
                         >
-                            <BookDisplay
-                                cover={book.cover}
-                                title={book.title}
-                                author={book.author}
-                            />
+                            <ShelfDisplay book={book} />
                         </div>
                     ))}
                 </div>
