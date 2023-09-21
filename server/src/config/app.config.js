079eb55d7b1eac84df04c037dab51ca6b7048ea0
Mackenzie Jessup <Mackenzie.jessup@mykenzie.snhu.edu>
2023-09-21 14:50:59 -0400

    added endpoints for signup/in

--- server/src/config/app.config.js
+++ server/src/config/app.config.js
@@ -1,2 +1,9 @@
 export const PORT = process.env.PORT || 3001;
 export const API_URL = process.env.API_URL || "/api";
+
+export default {
+  jwt: {
+    secret: process.env.JWT_SECRET || 'jwt-secret',
+    tokenLife: '7d',
+  },
+}
